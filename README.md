9131977c02c42b1488de9bf87cc0263fff1725dd
rrybak01 <rrybak01@educantabria.es>
2022-01-18 14:53:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-k2mvgc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-k2mvgc)
---
+++ src/components/Perfil.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Perfil() {
+  return (
+    <div className="main-site">
+      <h1>Página de perfil</h1>
+    </div>
+  );
+}
