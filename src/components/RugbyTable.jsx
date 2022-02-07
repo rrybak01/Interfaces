7f9244e2bd3b080d908050f3545f90da39dee299
rrybak01 <rrybak01@educantabria.es>
2022-02-07 09:27:59 +0100

    1 file added

---
+++ src/components/RugbyTable.jsx
@@ -0,0 +1,15 @@
+/*import React from 'react';
+import uuid from 'react-uuid';
+import { Card, Container, Table, Row, Col } from 'react-bootstrap';
+
+class RugbyTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItem: '',
+      selectedCountry: '',
+      selectedArena: '',
+      tableData: [],
+    };
+  }
+*/
